cfea8a9b69346db65e74c0f11f0e194cd4fb9994
ranatryambak <ranatryambak79@gmail.com>
2025-02-11 17:47:10 +0530

    updated script

--- creating_simple_UDP.py
+++ creating_simple_UDP.py
@@ -1,14 +1,23 @@
 import socket
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-msg = b'\xaa\xbb' + b'\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00' + b'\x06google\x03com\x00\x00\x01\x00\x01'
-address = ('8.8.8.8',53)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.sendto(msg,address)
 
-data,addr = sock.recvfrom(512)
+msg = b'\xaa\xbb' + b'\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00' + \
+      b'\x06google\x03com\x00' + b'\x00\x01' + b'\x00\x01'
 
-print(f"the data recieved from {addr} is {data.hex()}")
+address = ('8.8.8.8', 53)
+
+
+sock.sendto(msg, address)
+
+
+sock.settimeout(2)  
+try:
+    data, addr = sock.recvfrom(512)
+    print(f"Data received from {addr}: {data.hex()}")
+except socket.timeout:
+    print("Request timed out!")
 
 sock.close()
